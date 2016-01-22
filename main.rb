9c4b30eef15130fbec19f0939e04213e3e7c57b2
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-01-22 22:47:44 +0100

    Improve a little bit the display

--- main.rb
+++ main.rb
@@ -1,29 +1,29 @@
 require 'colorize'
 require_relative './model.rb'
 
-puts "#####################".green
-puts "Welcome to CloudMusic!".green
-puts "#####################\n".green
+puts "#########################".green
+puts "# Welcome to CloudMusic #".green
+puts "#########################\n".green
 
-puts "Enter artist name:"
+puts "Enter artist name:".white
 name = gets()
-puts "Searching for #{name.chomp}...\n\n"
+puts "Searching for #{name.chomp}...\n\n".white
 artist = get_artist(name.chomp)
 
-puts "### List of tracks for #{artist["full_name"]}: ###\n"
+puts "### List of tracks for #{artist["full_name"]}: ###\n".green
 
 id = artist["id"]
 tracks = get_artist_tracks(id)
-puts "There are #{tracks.length} tracks\n\n".magenta
+puts "There are #{tracks.length} tracks\n".magenta
 tracks.each_with_index do |track, index|
   puts "#{index} - #{track["title"]}".colorize(index%2 == 0 ? :blue : :cyan)
 end
 
-puts "Please choose a track to download by entering its ID"
+puts "Please choose a track to download by entering its ID".white
 track_index = gets()
 track_index = track_index.to_i
 
-puts "Downloading #{tracks[track_index]["title"]}..."
+puts "Downloading #{tracks[track_index]["title"]}...".white
 download_track(tracks[track_index]["stream_url"], tracks[track_index]["title"])
 
-puts "\n#{tracks[track_index]["title"]} has been downloaded!"
+puts "\n#{tracks[track_index]["title"]} has been downloaded!".green

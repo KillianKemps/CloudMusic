609aa03b9dfb4bf290ec3542d88564933707da55
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-01-27 14:19:41 +0100

    Add basic routes to artists

--- cloudmusic/config/routes.rb
+++ cloudmusic/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'artists#index'
+
+  resources :artists, only: [:index, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

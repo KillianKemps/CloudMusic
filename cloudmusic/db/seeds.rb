4561c5b39c931ef8c84af19ecc458e753961f630
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-01-27 15:37:28 +0100

    Add seed data

--- cloudmusic/db/seeds.rb
+++ cloudmusic/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artists = Artist.create([{ full_name: 'David Bowie', soundcloud_id: '000000' }, { full_name: 'Jeff Buckley', soundcloud_id: '000000' }])
+
+Track.create(title: 'Test track', artist_id: artists.first.id, stream_url: 'http://test.url')

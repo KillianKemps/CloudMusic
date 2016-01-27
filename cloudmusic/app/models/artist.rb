2ed4699f78acf07f03be8fd0d099cf3337c58535
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-01-27 14:36:27 +0100

    Remove useless class attributes

--- cloudmusic/app/models/artist.rb
+++ cloudmusic/app/models/artist.rb
@@ -1,13 +1,3 @@
 class Artist < ActiveRecord::Base
   has_many :tracks
-  attr_accessor :id, :full_name
-
-  def initialize(id, full_name)
-    @id = id
-    @soundcloud_id = soundcloud_id
-    @full_name = full_name
-    @avatar_url = avatar_url
-    @permalink_url = permalink_url
-    @track_count = track_count
-  end
 end
--- cloudmusic/app/models/track.rb
+++ cloudmusic/app/models/track.rb
@@ -1,13 +1,3 @@
 class Track < ActiveRecord::Base
   belongs_to :artist
-  attr_accessor :id, :title, :url
-
-  def initialize(id, title, stream_url)
-    @id = id
-    @title = title
-    @artist_id = artist_id
-    @stream_url = stream_url
-    @duration = duration
-    @artwork_url = artwork_url
-  end
 end
